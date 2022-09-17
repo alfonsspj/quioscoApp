f848af1b78e40465ebdee70df22f4e5194809617
alfons <alfonsspj@gmail.com>
2022-09-17 02:58:27 -0300

    Agregando layout principal

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,27 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+import Layout from '../layout/Layout';
 
-export default function Home({categorias}) {
-  console.log(categorias);// corre en la parte del cliente
+export default function Home() {
   return (
-    <h1>next.js</h1>
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
   )
 }
 
-//consulta la base de datos con prisma y next
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient()
+// //consulta la base de datos con prisma y next
+// export const getServerSideProps = async () => {
+//   const prisma = new PrismaClient()
   
-  // para que encuentre todas las categorias que hay en la bd
-  const categorias = await prisma.categoria.findMany();
+//   // para que encuentre todas las categorias que hay en la bd
+//   const categorias = await prisma.categoria.findMany();
 
-  // console.log(categorias)// en la parte del servidor
-  return {
-    props: {
-      categorias
-    }
-  }
-}
+//   // console.log(categorias)// en la parte del servidor
+//   return {
+//     props: {
+//       categorias
+//     }
+//   }
+// }
