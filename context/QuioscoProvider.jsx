66c5db9e87d742064b1b4cbdbb5e31e4239fcad7
alfons <alfonsspj@gmail.com>
2022-09-29 01:14:14 -0300

    Detectando el producto clickeado

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -7,6 +7,8 @@ const QuioscoProvider = ({children}) => {
 
     const [categorias, setCategorias] = useState([])
     const [categoriaActual, setCategoriaActual] = useState({})
+    const [producto, setProducto] = useState({})
+    const [modal, setModal] = useState(false)
 
     const obtenerCategorias = async () => {
         const { data } = await axios('/api/categorias')
@@ -27,12 +29,24 @@ const QuioscoProvider = ({children}) => {
         setCategoriaActual(categoria[0])
     }
 
+    // setear producto
+    const handleSetProducto = producto => {
+        setProducto(producto)
+    }
+    // modal
+    const handleChangeModal = () => {
+        setModal(!modal)
+    }
     return(
         <QuioscoContext.Provider
             value={{
                 categorias,
                 categoriaActual,
-                handleClickCategoria
+                handleClickCategoria,
+                producto,
+                handleSetProducto,
+                modal,
+                handleChangeModal
             }}
         >
             {children}
